d4e5642509229d5f9b8bf81d91cf77bbcb888345
mouphero411 <mouphero411@gmail.com>
2022-03-27 20:23:25 +0900

    Login Activity, SignUp Activity

---
+++ app/src/main/java/com/gudeok/gudeokapp/SignupActivity.kt
@@ -0,0 +1,24 @@
+package com.gudeok.gudeokapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.grade_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+    }
+}
