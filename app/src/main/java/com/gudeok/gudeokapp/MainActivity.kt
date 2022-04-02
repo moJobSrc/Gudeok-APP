f189f19ecafa5ce148e1dbadec6cfd901c186397
moJobSrc <>
2022-04-02 12:07:53 +0900

    update

--- app/src/main/java/com/gudeok/gudeokapp/MainActivity.kt
+++ app/src/main/java/com/gudeok/gudeokapp/MainActivity.kt
@@ -6,8 +6,7 @@ import com.gudeok.gudeokapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    //https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&pIndex=1&pSize=1&
-    //ATPT_OFCDC_SC_CODE=C10&SD_SCHUL_CODE=7150087&MLSV_FROM_YMD=20220327&MLSV_TO_YMD=20220327
+    //https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&pIndex=1&pSize=1&ATPT_OFCDC_SC_CODE=C10&SD_SCHUL_CODE=7150087&MLSV_FROM_YMD=20220327&MLSV_TO_YMD=20220327
 
     lateinit var binding: ActivityMainBinding
 
